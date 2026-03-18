24f1dc68342c397ad42b5d0bfdcf85f56bef3e9f
santhoshsandy6398-oss <santhoshsandy6398@gmail.com>
2026-03-18 18:06:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ transform = transforms.Compose([
     transforms.Resize((32, 32)),   # must match train.py
     transforms.ToTensor(),
     transforms.Normalize([0.5, 0.5, 0.5],
-                        [0.5, 0.5, 0.5)
+                        [0.5, 0.5, 0.5])
 ])
 
 @app.route("/")
